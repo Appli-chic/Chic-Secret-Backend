bed50f65f660c8bb2af562b56e7ffb5058f62cc5
Guillaume Belouin <guillaume.belouin@iltr.fr>
2021-07-20 12:36:33 +0200

    Update user model

--- model/user.go
+++ model/user.go
@@ -8,14 +8,16 @@ import (
 )
 
 type User struct {
-	ID          uuid.UUID    `gorm:"type:uuid;primary_key;"`
-	Email       string       `gorm:"type:varchar(255);unique_index"`
-	Tokens      []Token      `gorm:"foreignKey:UserID"`
-	LoginTokens []LoginToken `gorm:"foreignKey:UserID"`
-	Vaults      []Vault      `gorm:"foreignKey:UserID"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   *time.Time `sql:"index"`
+	ID           uuid.UUID    `gorm:"type:uuid;primary_key;"`
+	Email        string       `gorm:"type:varchar(255);unique_index"`
+	Tokens       []Token      `gorm:"foreignKey:UserID"`
+	LoginTokens  []LoginToken `gorm:"foreignKey:UserID"`
+	Vaults       []Vault      `gorm:"foreignKey:UserID"`
+	IsSubscribed bool         `gorm:"not null"`
+	Subscription string       `gorm:"type:varchar(255);not null"`
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	DeletedAt    *time.Time `sql:"index"`
 }
 
 func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
